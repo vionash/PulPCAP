6be4a734516b4cc5e7590009bc824cd72a58a51f
vionash <vladi@telnyx.com>
2020-03-11 19:14:53 -0500

    PulPCAP: Added explicit waits + cleanup

--- pulpcap.py
+++ pulpcap.py
@@ -1,37 +1,45 @@
 # PulPCAP
-# Vladi
-
-# Version 0.8
+# __Author__ = Vlad Ionash
+# Version 0.95
 
 import time
 import os
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.firefox.options import Options
-import threading
-from progress.spinner import PixelSpinner
-from selenium.webdriver.common.keys import Keys
-from progress.spinner import Spinner
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# List to show
+# classes TODO In reference to the check/wait, safely ignore for now
+class wait_for_text_to_start_with(object):
+    def __init__(self, locator, text_):
+        self.locator = locator
+        self.text = text_
+
+    def __call__(self, driver):
+        try:
+            element_text = EC._find_element(driver, self.locator).text
+            return element_text.startswith(self.text)
+        except StaleElementReferenceException:
+            return False
+
 KIBPListShow = ["CH1", "DC2"]
-
-# List to reference in code
 KIBPList = [x.lower() for x in KIBPListShow]
-
 name = input("Enter Name:")
-
 UID = [str(x).strip() for x in input(
-            "Enter Telnyx Call Leg ID (Note: If you'd like to do more than 1, simply separate them with a space!):").split()]
+    "Enter Telnyx Call Leg ID (Note: If you'd like to do more than 1, simply separate them with a space!):").split()]
 
 # Enviro Variable from bash script ^^^ import OS
 try:
-    KIBPLoc = (os.environ['KIBPLoc'])
-    print("Reference: Current KIBP Tanker is: " + KIBPLoc)
+    KIBPLocation = (os.environ['KIBPLocation'])
+    print("\n" + "Reference: Current KIBP Tanker is: " + KIBPLocation)
 except:
-    print("Hmmm...Looks like we couldn't get the current KIBP from the traceroute, hope you know it!")
+    print("\n" + "Hmmm...Looks like we couldn't get the current KIBP from the traceroute, hope you know it!")
 
-print("List of SIP Tankers" + str(KIBPListShow) + ":")
+print("\n" + "List of SIP Tankers" + str(KIBPListShow) + ":")
 
+#KIBP Input
 while True:
     try:
         KIBP = input("Enter SIP KIBP Location from list above! (If left blank, defaults to CH1):").strip()
@@ -42,27 +50,28 @@ while True:
     if KIBP in KIBPList or KIBP == "":
         break
     else:
+        print("")
         print(KIBPListShow)
         print("Not a valid input! Choose from above or leave blank!")
         continue
 
 KIBP = KIBP.upper()
 if KIBP == "":
-    print("Using CH1 location!")
+    print("\n" + "Using CH1 location!")
 else:
-    print("Using " + KIBP + " location!")
+    print("\n" + "Using " + KIBP + " location!")
 
-#Start webdriver! Using GeckoDriver(Firefox), run headleslly
+# Start webdriver! Using GeckoDriver(Firefox), run headlessly
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
 
 """
-#########IF YOU"RE LOOKING FOR PATH TO WEBDRIVER, IT IS HERE!!!###########
+############################IF YOU"RE LOOKING FOR PATH TO WEBDRIVER, IT IS HERE!!!######################################
 # If you want to specify driver location, simply append the above driver with (executable_path=r'<Your.path.here.exe')
 # Example:
-# driver = webdriver.Firefox(options=options, executable_path=r'C:\WebDriver\bin\geckodriver.exe')
-##########################################################################
+#         driver = webdriver.Firefox(options=options, executable_path=r'C:\WebDriver\bin\geckodriver.exe')
+########################################################################################################################
 """
 
 # functions
@@ -88,6 +97,7 @@ def PCAP_RTP(UID):
     elem.click()
 
     time.sleep(2)
+    # TODO Program check instead of wait... don't think this is possible
 
     # Name
     elem = driver.find_element_by_xpath(
@@ -100,9 +110,15 @@ def PCAP_RTP(UID):
         "/html/body/div/main/div[2]/form/button")
     elem.click()
 
-    time.sleep(2)
+    # Wait for output message and grab it
+    try:
+        elem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[2]/span/span"))
+        )
+    except ValueError:
+        print("Error 532: We Will Not Submit!!!")
 
-    # Grab the output message
     messageRTP = driver.find_element_by_xpath(
         "/html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[2]/span/span")
     firstTenMessageRTP = messageRTP.text[1:14]
@@ -139,8 +155,9 @@ def PCAP_SIP(UID):
     elem.click()
 
     time.sleep(2)
+    # TODO 2nd Wait
 
-    # Host to CH1
+    # Host to KIBP, if blank defaults to CH1. Add elifs to add more KIBPs. More KIBP
     if KIBP == "ch1":
         elem = driver.find_element_by_xpath(
             "/html/body/div/main/div[2]/form/div[2]/select/option[4]").click()
@@ -166,15 +183,24 @@ def PCAP_SIP(UID):
         "/html/body/div/main/div[2]/form/button")
     elem.click()
 
-    time.sleep(2)
+    # Wait for output message and grab it
+    try:
+        elem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[2]/span/span"))
+        )
+    except ValueError:
+        print("Error 532: We Will Not Submit!!!")
 
-    # Grab the output message
     messageSIP = driver.find_element_by_xpath(
         "/html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[2]/span/span")
     firstTenMessageSIP = messageSIP.text[1:14]
 
     if firstTenMessageSIP == "start_time or":
         print("Hmmm...For the SIP portion, either wrong filter or doesn't exist! Double check ID")
+    elif firstTenMessageSIP == "invalid_filter":
+        print("Call doesn't look like it exists, isn't populated yet, or just an invalid filter. "
+              "I would try doing it manually if you believe this is a mistake.")
     else:
         print("SIP " + messageSIP.text)
 
@@ -184,6 +210,4 @@ for X in UID:
     PCAP_RTP(X)
 
 driver.close()
-
 print("Congrats! You are lazy. Or attempted to be.")
-# elem.send_keys(Keys.RETURN)
--- pulpcap.sh
+++ pulpcap.sh
@@ -3,9 +3,9 @@
 #Tracerouting to KIBP USA IP, storing as TR
 TR=$(traceroute 10.255.0.1)
 #Grabs tel-, and 16 digits after.
-KIBPLoc=$(echo $TR | grep -o -P '.{0,0}tel-.{0,16}')
+KIBPLocation=$(echo $TR | grep -o -P '.{0,0}tel-.{0,16}')
 #echo $KIBPLoc
 
-#export to environ variable
-export KIBPLoc
+#export to enviro variable
+export KIBPLocation
 python pulpcap.py
